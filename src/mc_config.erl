f0f87e7fd4eb05156f9bf204c270fcb3256619c8
Martin Huber <martinh@innoq.com>
2011-06-30 18:16:13 +0200

    started api for musiccrawler_config

--- src/mc_config.erl
+++ src/mc_config.erl
@@ -1,4 +1,4 @@
-%% Copyright 2010 Ulf Angermann
+%% Copyright 2011 Ulf Angermann / Martin Huber
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
 %% limitations under the License.
 
 %%% -------------------------------------------------------------------
-%%% Author  : Ulf Angermann uaforum1@googlemail.com
+%%% Author  : Martin Huber / martin.huber@innoq.com
 %%% Description :
 %%%
 %%% Created : 
@@ -25,7 +25,9 @@
 %% Include files
 %% --------------------------------------------------------------------
 -include_lib("eunit/include/eunit.hrl").
-
+-define(STATION_CONFIG, filename:join([code:priv_dir("musiccrawler"), "rstations.config"])).
+-define(MUSICLIST_CONFIG, filename:join([code:priv_dir("musiccrawler"), "musiclist.config"])).
+-define(GENRE_CONFIG, filename:join([code:priv_dir("musiccrawler"), "genres.config"])).
 %% --------------------------------------------------------------------
 %% External exports
 
@@ -41,7 +43,10 @@
 %% --------------------------------------------------------------------
 %% record definitions
 %% --------------------------------------------------------------------
--record(state, {}).
+-record(	state, 
+					{genres, 
+					 stations, 
+					 musiclist}).
 %% ====================================================================
 %% Server functions
 %% ====================================================================
@@ -54,6 +59,14 @@ start_link() ->
 
 start() ->
 	start_link().
+
+%%--------------------------------------------------------------------
+%% @doc Stops the server.
+%% @spec stop() -> ok
+%% @end
+%%--------------------------------------------------------------------
+stop() ->
+    gen_server:cast(?MODULE, stop).
 %% --------------------------------------------------------------------
 %% Function: init/1
 %% Description: Initiates the server
@@ -63,21 +76,32 @@ start() ->
 %%          {stop, Reason}
 %% --------------------------------------------------------------------
 init([]) ->
-    {ok, #state{}}.
+	{ok, [Stations]} = file:consult(?STATION_CONFIG),
+	{ok, [MusicL]} = file:consult(?MUSICLIST_CONFIG),
+	{ok, [ValidGenres]} = file:consult(?GENRE_CONFIG),
+	true = check_stations(Stations, ValidGenres),
+    {ok, #state{genres=ValidGenres, stations=Stations, musiclist=MusicL}}.
 
 %% --------------------------------------------------------------------
 %% Function: handle_call/3
-%% Description: Handling call messages
+%% Args:	{stations} -> lists all configured stations as record-list
+%% Description: lists all configured stations as record-list
 %% Returns: {reply, Reply, State}          |
-%%          {reply, Reply, State, Timeout} |
-%%          {noreply, State}               |
-%%          {noreply, State, Timeout}      |
-%%          {stop, Reason, Reply, State}   | (terminate/2 is called)
-%%          {stop, Reason, State}            (terminate/2 is called)
 %% --------------------------------------------------------------------
-handle_call(Request, From, State) ->
-    Reply = ok,
+handle_call({stations}, From, State) ->
+    Reply = State#state.stations,
+    {reply, Reply, State};
+%% --------------------------------------------------------------------
+%% Function: handle_call/3
+%% Args:	{stations, [Genres]} ->  
+%% Description: Handling call messages, lists all configured stations, that are in the given genre-types-list
+%% Returns: {reply, Reply, State}          |
+%% --------------------------------------------------------------------
+
+handle_call({stations, Genre}, From, State) ->
+    Reply = get_stations_by_genre(State#state.stations, Genre),
     {reply, Reply, State}.
+
 
 %% --------------------------------------------------------------------
 %% Function: handle_cast/2
@@ -118,7 +142,39 @@ code_change(OldVsn, State, Extra) ->
 %% --------------------------------------------------------------------
 %%% Internal functions
 %% --------------------------------------------------------------------
+
+check_stations(Stations, Genres) ->	
+	BoolList=[lists:member(K, Genres) || {_,K,_,_,_}<-Stations],
+	not lists:member(false, BoolList).
+	
+get_stations_by_genre(Stations, Genres) ->
+	[K || K = {_,X2,_,_,_} <- Stations, lists:member(X2, Genres)].
+
 %% --------------------------------------------------------------------
 %%% Test functions
 %% --------------------------------------------------------------------
 
+startstop_test() -> 
+	start(),
+	stop().
+	
+
+check_stations_test() ->
+	Stations= [{x, genre1, x, x, x }, {x, genre2, x, x, x }, {x, genre1, x, x, x } ],
+	StationsWrong= [{x, genre31, x, x, x }, {x, genre2, x, x, x}, {x, genre1, x, x, x }],
+	Genres = [genre1, genre2],
+	true = check_stations(Stations, Genres),
+	false = check_stations(StationsWrong, Genres),
+	ok.
+	
+get_stations_bygenre_test() ->
+	Stations= [{x, genre1, x, x, x }, {x, genre2, x, x, x }, {x, genre1, x, x, x }, {x, genre1, x, x, x }, {x, genr31, x, x, x } ],
+	Genres = [genre1],									% Test with only one genre
+	L = get_stations_by_genre(Stations, Genres),
+	true = check_stations(L, Genres),					% All found of Correct genre
+	Genres2 = [genre1, genre2],							% Test with multiple genres
+	L2 = get_stations_by_genre(Stations, Genres),
+	true = check_stations(L2, Genres2),					% All found of Correct genre
+	[] = get_stations_by_genre(Stations, [wronggenre]),   % Empty when a wrong genre is given
+	
+	ok.
